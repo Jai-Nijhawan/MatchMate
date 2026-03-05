32fe2f488e94ea6ff85d71bb367ca99cf211024b
Jai Nijhawan <jainijhawan@gmail.com>
2026-03-05 13:43:00 +0530

    Initial Commit

---
+++ MatchMate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MatchMate
+//
+//  Created by Jai Nijhawan on 05/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("HI")
+        }
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
